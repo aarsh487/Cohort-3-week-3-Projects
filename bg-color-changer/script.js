4cc629ad1c0739c0be42b6f3963002739a81bed8
aarsh487 <160403377+aarsh487@users.noreply.github.com>
2024-10-03 23:11:40 +0530

    Add files via upload

---
+++ bg-color-changer/script.js
@@ -0,0 +1,48 @@
+function changeRed(){
+    cRed = document.querySelector('body')
+    cRed2 = document.querySelector('.main')
+    cRed.style.backgroundColor = 'Red'
+    cRed2.style.backgroundColor = 'Red'
+}
+
+function changeYellow(){
+    cRed = document.querySelector('body')
+    cRed2 = document.querySelector('.main')
+    cRed.style.backgroundColor = 'Yellow'
+    cRed2.style.backgroundColor = 'Yellow'
+}
+
+function changeBlack(){
+    cRed = document.querySelector('body')
+    cRed2 = document.querySelector('.main')
+    cRed.style.backgroundColor = 'Black'
+    cRed2.style.backgroundColor = 'Black'
+}
+
+function changePurple(){
+    cRed = document.querySelector('body')
+    cRed2 = document.querySelector('.main')
+    cRed.style.backgroundColor = 'Purple'
+    cRed2.style.backgroundColor = 'Purple'
+}
+
+function changeGreen(){
+    cRed = document.querySelector('body')
+    cRed2 = document.querySelector('.main')
+    cRed.style.backgroundColor = 'Green'
+    cRed2.style.backgroundColor = 'Green'
+}
+
+function changeBlue(){
+    cRed = document.querySelector('body')
+    cRed2 = document.querySelector('.main')
+    cRed.style.backgroundColor = 'Blue'
+    cRed2.style.backgroundColor = 'Blue'
+}
+
+function changeDefault(){
+    cRed = document.querySelector('body')
+    cRed2 = document.querySelector('.main')
+    cRed.style.backgroundColor = 'Grey'
+    cRed2.style.backgroundColor = 'Grey'
+}
